813b9b9da4c074afb59fb62b1ad7324abcb99ee0
JWSong <thdwjddn123@gmail.com>
2020-12-01 17:01:54 +0900

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='python-upbit',
     packages=['upbit'],
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='A python wrapper for UPBit',
     long_description=long_description,
